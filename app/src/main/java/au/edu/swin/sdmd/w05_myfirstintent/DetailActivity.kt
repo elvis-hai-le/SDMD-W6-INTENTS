00442ebc3f90783bac4e8c54754ac589cbb2de72
Elvis Le <elvishaile120@gmail.com>
2024-05-02 17:03:29 +1000

    Updated to add multiplication task passing result via intent

--- app/src/main/java/au/edu/swin/sdmd/w05_myfirstintent/DetailActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w05_myfirstintent/DetailActivity.kt
@@ -1,11 +1,19 @@
 package au.edu.swin.sdmd.w05_myfirstintent
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val showResult = findViewById<TextView>(R.id.productResult)
+        val answer = intent.getStringExtra("result")
+        Log.i("result","$answer")
+
+        showResult.text = answer
     }
 }
--- app/src/main/java/au/edu/swin/sdmd/w05_myfirstintent/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w05_myfirstintent/MainActivity.kt
@@ -3,7 +3,9 @@ package au.edu.swin.sdmd.w05_myfirstintent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 /**
  * Your task: create an intent that opens the DetailActivity when the image in MainActivity
@@ -20,9 +22,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image = findViewById<ImageView>(R.id.mainImage)
-        image.setOnClickListener {
+        val product1 = Random.nextInt(from = 1, until = 13)
+        val product2 = Random.nextInt(from = 1, until = 13)
+
+        val showP1 = findViewById<TextView>(R.id.productOne)
+        val showP2 = findViewById<TextView>(R.id.productTwo)
+
+        val result = product1 * product2
+
+        showP1.text = product1.toString()
+        showP2.text = product2.toString()
+
+        val multiply = findViewById<Button>(R.id.multiplyBtn)
+        multiply.setOnClickListener {
             val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("result", result.toString())
             startActivity(intent)
         }
 
